8bf3800fa57c866ca6d3dc2af9878e5dcf29b5ff
arpit57 <arpit.singhal57@gmail.com>
2024-02-26 17:52:45 +0530

    added ssl files and updated main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000,log_level="info",reload=True)
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        ssl_keyfile="/home/arpit57/countApp/app/ssl_key.pem",
+        ssl_certfile="/home/arpit57/countApp/app/ssl_certificate.pem"
+    )
